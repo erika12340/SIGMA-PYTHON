d5321c268ab0c686b25b0bb34a6c983a034f2f8f
erika12340 <erikarachmawatiixg@gmail.com>
2025-08-27 16:35:32 +0700

    kirim code terbaru

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # SIGMA
 
 ## SETUP
- - pip install -r requirements.txt
- - pip freeze > requirements.txt
+
+- python -m venv env
+- env\scripts\activate
+- pip install -r requirements.txt
+- pip freeze > requirements.txt
+- python manage.py runserver
+
+## database
+
+- pip install python-decouple
--- data_materials/settings.py
+++ data_materials/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -64,19 +65,18 @@ WSGI_APPLICATION = 'data_materials.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-     'default': {
-        'ENGINE':'mssql',
-        'NAME':'PCS',
-        'USER':'dev',
-        'PASSWORD':'er123',
-        'HOST' : 'localhost',
-        'PORT':'1433',
+    'default': {
+        'ENGINE': config('DB_ENGINE'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT', cast=int),
         'OPTIONS': {
             'driver': 'ODBC Driver 17 for SQL Server',
         },
     }
 }
-
 
 
 # Password validation
